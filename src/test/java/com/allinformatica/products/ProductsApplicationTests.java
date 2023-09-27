b785b3aeb178bd432bd5e2db973b030079103f63
Luciano Nascimento <luciano.m.nasc@gmail.com>
2023-09-27 07:55:07 -0300

    first commit

---
+++ src/test/java/com/allinformatica/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinformatica.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
